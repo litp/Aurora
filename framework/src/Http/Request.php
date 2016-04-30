7fb81026a8def1f59f75b8efb0b1e96c17aff564
litianpeng200 <litianpeng200@gmail.com>
2016-04-30 23:39:18 +0800

    updated Request.php

--- framework/src/Http/Request.php
+++ framework/src/Http/Request.php
@@ -2,106 +2,114 @@
 
 class Request
 {
-	protected static $instance;
+    protected static $instance;
 
-	public $scheme;
-	public $body;
-	public $path;
-	public $method;
-	public $encoding;
+    public $scheme;
+    public $body;
+    public $path;
+    public $method;
+    public $encoding;
 
-	public $GET;
-	public $POST;
-	public $COOKIES;
-	public $FILES;
-	public $META;
+    public $GET;
+    public $POST;
+    public $COOKIES;
+    public $FILES;
+    public $META;
 
-	public static function create()
-	{
-		if (empty(self::$instance)) {
-			self::$instance = self::constructFromEnvironment();
-		}
+    public static function create()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = self::constructFromEnvironment();
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 
-	public static function constructFromEnvironment()
-	{
-		self::$instance = new Request(	$_GET,
-										$_POST,
-										$_COOKIES,
-										$_FILES,
-										$_SERVER);
-		return self::$instance;
-	}
+    public static function constructFromEnvironment()
+    {
+        self::$instance = new Request(  $_GET,
+                                        $_POST,
+                                        $_COOKIES,
+                                        $_FILES,
+                                        $_SERVER);
+        return self::$instance;
+    }
 
-	public function __construct($get = array(),
-								$post = array(),
-								$cookies = array(),
-								$files = array(),
-								$servers = array())
-	{
-		$this->GET = $get;
-		$this->POST = $post;
-		$this->COOKIES = $cookies;
-		$this->FILES = $files;
-		$this->META = $servers;
+    public function __construct($get = array(),
+                                $post = array(),
+                                $cookies = array(),
+                                $files = array(),
+                                $servers = array())
+    {
+        $this->GET = $get;
+        $this->POST = $post;
+        $this->COOKIES = $cookies;
+        $this->FILES = $files;
+        $this->META = $servers;
 
-		$this->setScheme();
-		$this->setPath();
-		$this->setMethod();
-		$this->setEncoding();
-	}
+        $this->setScheme();
+        $this->setPath();
+        $this->setMethod();
+        $this->setEncoding();
+    }
 
 
 
-	public function getHost()
-	{
-		//
-	}
+    public function getHost()
+    {
+        return $this->META['HTTP_HOST'];
+    }
 
-	public function getPort()
-	{
-		//
-	}
+    public function getPort()
+    {
+        return $this->META['SERVER_PORT'];
+    }
 
-	public function getFullPath()
-	{
+    public function getFullPath()
+    {
+        return ;
+    }
 
-	}
+    public function buildAbsoluteUri()
+    {
+        //
+    }
 
-	public function buildAbsoluteUri()
-	{
-		//
-	}
+    public function isSecure()
+    {
+        //
+    }
 
-	public function isSecure()
-	{
-		//
-	}
+    public function isAjax()
+    {
+        //
+    }
 
-	public function isAjax()
-	{
-		//
-	}
+    public function setScheme($scheme = 'http')
+    {
+        if (isset($this->META['REQUEST_SCHEME'])) {
+            $this->scheme = $this->META['REQUEST_SCHEME'];
+        } else {
+            $this->scheme = $scheme;
+        }
+    }
 
-	public function setScheme()
-	{
-		//
-	}
+    public function setPath($path = null)
+    {
+        //
+    }
 
-	public function setPath()
-	{
-		//
-	}
+    public function setMethod($method = 'GET')
+    {
+        if (isset($this->META['REQUEST_METHOD'])) {
+            $this->method = $this->META['REQUEST_METHOD'];
+        } else {
+            $this->method = $method
+        }
+    }
 
-	public function setMethod()
-	{
-		//
-	}
-
-	public function setEncoding()
-	{
-		//
-	}
+    public function setEncoding()
+    {
+        //
+    }
 }
