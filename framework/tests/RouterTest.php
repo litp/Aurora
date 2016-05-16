6a9ded6af7831a0c86d6224756f07cea525b1a54
litianpeng200 <litianpeng200@gmail.com>
2016-05-16 23:31:36 +0800

    added RouterTest file

---
+++ framework/tests/RouterTest.php
@@ -0,0 +1,71 @@
+<?php
+
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+    protected $request;
+    protected $router;
+    protected $routerTable = array(
+            '/test' => array('TestController', 'test')
+        );
+
+
+    public function testEscapeRouterTable()
+    {
+        $this->router = new Router($this->routerTable);
+
+        $testTable = array(
+            '/test' => 'test'    
+        );
+
+        $escaped = $this->router->escapeRouterTable($testTable);
+
+        $this->assertArrayHasKey("/\/test/", $escaped);
+    }
+
+    public function testGetQueryPath()
+    {
+        $this->router = new Router($this->routerTable);
+
+        $this->request = new Request(
+            'GET',              //method
+            '/index.php/test',  //requestUri
+            'HTTP/1.1',         //version
+            array(),            //headers
+            '',                 //body
+            array(),            //cookies
+            array(),            //GET
+            array(),            //POST
+            array(              // serverInfo
+                'scriptName' => '/index.php'
+                )             
+            );
+
+        $result = $this->router->getQueryPath($this->request);
+
+        $this->assertEquals('/test', $result);
+    }
+
+    public function testDispatch()
+    {
+        $this->routerTable = array(
+            '/test' => array('TestController', 'test')
+        );
+
+        $this->request = new Request(
+            'GET',              //method
+            '/index.php/test',  //requestUri
+            'HTTP/1.1',         //version
+            array(),            //headers
+            '',                 //body
+            array(),            //cookies
+            array(),            //GET
+            array(),            //POST
+            array(              // serverInfo
+                'scriptName' => '/index.php'
+                )             
+            );
+        $this->router = new Router($this->routerTable);
+
+        $this->router->dispatch($this->request);
+    }
+}
