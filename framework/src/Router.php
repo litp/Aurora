ff2fe7bd641b98eefc411a392072e75cba95219a
litianpeng200 <litianpeng200@gmail.com>
2016-05-16 23:30:47 +0800

    added initial methods

--- framework/src/Router.php
+++ framework/src/Router.php
@@ -1,31 +1,47 @@
 <?php
 class Router
 {
-    protected $directRouterTable = [
-    // 'url' => function
-    '/\/register/' => ['Registration', 'register']
-    ];
-    protected $rootDirectory = __DIR__;
+    protected $directRouterTable = array();
+    protected $rootDirectory = '';  //unused yet!!
     protected $controllerInstance;
     
-    // return query path starting with '/' and without the last '/'
-    public function getQueryPath()
+    public function __construct($routerTable = array())
     {
-        if (isset($_SERVER['REQUEST_URI']) && isset($_SERVER['SCRIPT_NAME'])) {
-            $queryPath = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['REQUEST_URI']);
-            if ($queryPath != '') {
-                return $queryPath;
-            } 
-        }
-        return '/';
+        $this->directRouterTable = $this->escapeRouterTable($routerTable);
     }
-    public function dispatch($queryPath = null)
+
+    public function escapeRouterTable($routerTable)
     {
-        if (is_null($queryPath)) {
-            $queryPath = $this->getQueryPath();
+        $escapedRouterTable = array();
+
+        foreach ($routerTable as $regex => $function) {
+            $escapedRouterTable['/' . addcslashes($regex, "\'\"\\\/") . '/'] = $function;
         }
-        // first check if query path matches direct router table
-        foreach ($this->$directRouterTable as $uri => $function) {
+
+        return $escapedRouterTable;
+    }
+
+    // return query path starting with '/' and without the last '/'
+    public function getQueryPath($request)
+    {
+        if (isset($request) && 
+            isset($request->serverInfo->scriptName) &&
+            isset($request->requestUri)) {
+            
+            $queryPath = str_replace($request->serverInfo->scriptName, '', $request->requestUri);
+
+            return empty($queryPath) ? '/' : $queryPath;
+
+        } else {
+            return '/';
+        }
+    }
+
+    public function dispatch($request = null)
+    {
+        $queryPath = $this->getQueryPath($request);
+
+        foreach ($this->directRouterTable as $uri => $function) {
             if (preg_match($uri, $queryPath, $matches)) {
                 $parameterPath = str_replace($matches[0],'',$queryPath);
                 if ($parameterPath == '') {
@@ -34,32 +50,8 @@ class Router
                     $parameters = explode('/',rtrim($parameterPath, '/'));
                     array_shift($parameters);
                 }
-                //var_dump($function);
-                //var_dump($parameters);
+
                 return call_user_func($function,$parameters);
-            }
-        }
-        // try to load the class
-        $queryPath = rtrim($queryPath, '/');
-        $queryPathParts = explode('/', $queryPath);
-        $classPath = $this->rootDirectory;
-        // remove the first empty element
-        array_shift($queryPathParts);
-        foreach ($queryPathParts as $key => $queryPart) {
-            if (file_exists($classPath . '/' . $queryPart) && 
-                is_dir($classPath . '/' . $queryPart)) {
-                    $classPath = $classPath . '/' . $queryPart;
-                    var_dump($classPath);
-            } elseif (file_exists($classPath . '/' . $queryPart . '.php') && 
-                is_file($classPath . '/' . $queryPart . '.php')) {
-                //load the class in the file and call the function
-                require_once($classPath . '/' . $queryPart . '.php');
-                $this->controllerInstance = new $queryPart;
-                //var_dump(array($this->controllerInstance, $queryPart));
-                //var_dump(array_slice($queryPathParts, $key+1));
-                return call_user_func(array($this->controllerInstance, $queryPart), array_slice($queryPathParts, $key+1));               
-            } else {
-                // throw Bad Request Exception
             }
         }
     }
